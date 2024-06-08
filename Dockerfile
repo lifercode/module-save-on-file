06b1364da6e7c5a27e302e0a229e11a135566047
Andre <lifercode@gmail.com>
2024-06-08 18:43:02 -0300

    update compose

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM node:18
 WORKDIR /usr/src/app
+RUN mkdir -p /usr/src/app/files
 COPY package*.json *.js ./
 RUN npm install
 EXPOSE 5555
--- save-file.js
+++ save-file.js
@@ -1,12 +1,12 @@
 const fs = require('fs/promises')
 
-const saveFile = async (
+const saveFile = async ({
   filename,
   extension,
   content
-) => {
+}) => {
   try {
-    await fs.writeFile(`${filename}.${extension}`, content);
+    await fs.writeFile(`./files/${filename}.${extension}`, content);
     return `${filename}.${extension}`
   } catch (error) {
     console.error(error?.message)
