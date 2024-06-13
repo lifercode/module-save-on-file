9c3f18fa66d3e262e477cacf8f8822e3706b343b
Andre <lifercode@gmail.com>
2024-06-13 09:48:45 -0300

    save file register db

--- app.js
+++ app.js
@@ -13,16 +13,37 @@ app.get("/", (req, res) => {
 });
 
 app.post("/save-file", async (req, res) => {
-    const result = await saveFile({
-      filename: req?.body?.filename || '',
-      extension: req?.body?.extension || 'txt',
-      content: req?.body?.content || ''
+  try {
+    const execNode = req?.body?.execNodeId
+    const drive = req?.body?.driveId
+    const owner = req?.body?.userId
+    const inputsData = req?.body?.inputsData
+  
+    const filename = inputsData?.filename || ''
+    const extension = inputsData?.extension || 'txt'
+    const content = inputsData?.content || ''
+    const name = `${filename}.${extension}`
+  
+    const result = await saveFile({ filename, extension, content })
+
+    const payload = { execNode, drive, owner, name, filename, extension }
+  
+    const resultNodes = await fetch(`${process.env.API_URL}/files`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload)
     })
-    res.json({
-      filename: result
-    })
+
+    const resultJsonNodes = await resultNodes?.json()
+
+    res.json({ resultJsonNodes, filename: result })
+  } catch (error) {
+    res.status(500).json({ error })
+  }
 });
 
 app.listen(5555, () => {
-    console.log("server started on port 5555");
+  console.log("server started on port 5555");
 });
